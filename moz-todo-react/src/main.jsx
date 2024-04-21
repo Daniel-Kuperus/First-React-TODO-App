fe87187788dbbdec963d1ba43a83913d512be26a
Daniel Kuperus <daniel.kuperus@gmail.com>
2024-04-21 17:55:29 +0200

    Added another task.

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -6,8 +6,9 @@ import './index.css'
 
 const DATA = [
   { id: "todo-0", name: "Flunk a React tutorial", completed: true },
-  { id: "todo-1", name: "Give Ollie a snack", completed: true },
-  { id: "todo-2", name: "Try making Minesweeper using React", completed: false },
+  { id: "todo-1", name: "Add random photos of dogs", completed: true },
+  { id: "todo-2", name: "Give Ollie a snack", completed: false },
+  { id: "todo-3", name: "Try making Minesweeper using React", completed: false },
 ];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
